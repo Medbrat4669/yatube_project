4822010d6078b73e55f60fa6efdf2ad92f316ab6
Чугин Владислав <210nk@mail.ru>
2022-07-19 12:32:19 +0300

    Обновил ссылки и отредактировал информацию

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
-    path('', views.index),
-    path('group/<slug:slug>/', views.group_posts)
+    path('posts/', views.group_list, name='group_list'),
+    path('index.html', views.index, name='index'),
+    path('', views.index, name='index')
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    title1 = 'Это главная страница проекта Yatube'
+    context = {
+        'title1': title1
+    }
+    return render(request, template, context) 
 
 
-def group_posts(request, slug):
-    return HttpResponse(f'Страница сообщества {slug}')
+def group_list(request):
+    template = 'posts/group_list.html'
+    title2 = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'title2': title2
+    }
+    return render(request, template, context)
+
+
