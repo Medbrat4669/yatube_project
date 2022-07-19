a365ef67e2e47b5d956e2ff336e78d272f37147c
Чугин Владислав <210nk@mail.ru>
2022-07-19 21:48:17 +0300

    доделал дизайн

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,16 +4,16 @@ def index(request):
     template = 'posts/index.html'
     title1 = 'Это главная страница проекта Yatube'
     context = {
-        'title1': title1
+        'title1':title1
     }
     return render(request, template, context) 
 
 
-def group_list(request):
+def group_posts(request):
     template = 'posts/group_list.html'
     title2 = 'Здесь будет информация о группах проекта Yatube'
     context = {
-        'title2': title2
+        'title2':title2
     }
     return render(request, template, context)
 
