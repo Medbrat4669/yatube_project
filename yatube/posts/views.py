b1af9e60de41d860551ec4ac9dc27c30a6c04201
Чугин Владислав <210nk@mail.ru>
2022-07-20 22:38:31 +0300

    добавил в функция index "post"

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
+from .models import Post
 
 def index(request):
-    template = 'posts/index.html'
+    posts = Post.objects.order_by('-pub_date')[:10]
     title1 = 'Это главная страница проекта Yatube'
     context = {
+        'posts': posts,
         'title1':title1
     }
-    return render(request, template, context) 
+    return render(request, 'posts/index.html', context) 
 
 
 def group_posts(request):
