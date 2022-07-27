439bfcf12bbafb2e65b780a834800a04ad934a85
Medbrat4669 <210nk@mail.ru>
2022-07-27 17:27:03 +0300

    Финал версия

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import get_object_or_404, render
+from django.conf import settings
 
 from .models import Post, Group
 
 
 def index(request):
-    posts = Post.objects.all()[:10]
+    posts = Post.objects.all()[:settings.SORT_POSTS]
     template = 'posts/index.html'
     context = {
         'posts': posts,
@@ -15,7 +16,7 @@ def index(request):
 def group_posts(request, slug):
     template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group)[:10]
+    posts = Post.objects.filter(group=group)[:settings.SORT_POSTS]
     context = {
         'group': group,
         'posts': posts,
