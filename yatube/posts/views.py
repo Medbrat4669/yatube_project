388094c05c0cb29893e5bae8982209a05959c73d
Medbrat4669 <210nk@mail.ru>
2022-08-07 22:47:32 +0300

    neww

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -28,7 +28,7 @@ def index(request):
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = group.groups.select_related('author')
-    page_obj =  paginator(request, posts, settings.SORT_POSTS)
+    page_obj = paginator(request, posts, settings.SORT_POSTS)
     template = 'posts/group_list.html'
     context = {
         'group': group,
