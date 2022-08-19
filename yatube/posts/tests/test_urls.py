4afa94b80e28303712f881effc6665a64e2fde05
Medbrat4669 <210nk@mail.ru>
2022-08-20 00:28:10 +0300

    o

--- yatube/posts/tests/test_urls.py
+++ yatube/posts/tests/test_urls.py
@@ -4,18 +4,11 @@ from http import HTTPStatus
 
 from ..models import Group, Post, User
 
-''' 
-Я сделал как ты  просил, чтобы  автор  побегал по страница
-Оставил все тесты как были, но  хз правильно ли, что ошибки показывать будет по тестам
-анонимного пользователя например
-(в принципе функционал же ошибка сможет показать )
-''' 
 TEST_USERNAME = 'test-user'
 TEST_POST_TEXT = 'Тест текст поста'
 TEST_GROUP_TITLE = 'Тест группа'
 TEST_GROUP_SLUG = 'test-slug'
 TEST_GROUP_DESCRIPTION = 'Тест описание группы'
-#TEST_USER_NON_AUTHOR_USERNAME = 'test-empty-user'
 
 
 class PostURLTests(TestCase):
@@ -59,7 +52,6 @@ class PostURLTests(TestCase):
         self.post_author = Client()
         self.post_author.force_login(PostURLTests.user)
 
-
     def test_public_pages_exist_at_desired_locations(self):
         """
         Общедоступные страницы пользователя
@@ -80,48 +72,12 @@ class PostURLTests(TestCase):
             reverse('posts:post_create'))
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
-    def test_post_create_page_redirect_anonymous_on_login_page(self):
-        """
-        Страница /create/ перенаправит анонимного пользователя
-        на страницу логина.
-        """
-        response = self.post_author.get(
-            reverse('posts:post_create'))
-        self.assertRedirects(
-            response,
-            f"{reverse('users:login')}?next={reverse('posts:post_create')}")
-
     def test_post_edit_page_exists_at_desired_location_author(self):
         """Страница /<post_id>/edit/ доступна автору поста."""
         response = self.post_author.get(
             reverse(
                 'posts:post_edit', kwargs={'post_id': PostURLTests.post.id}))
         self.assertEqual(response.status_code, HTTPStatus.OK)
-
-    def test_post_edit_page_redirect_non_author_on_post_detail_page(self):
-        """
-        Страница /<post_id>/edit/ перенаправит пользователя, не являющегося
-        автором поста, на страницу с информацией о данном посте.
-        """
-        response = self.post_author.get(
-            reverse(
-                'posts:post_edit', kwargs={'post_id': PostURLTests.post.id}))
-        self.assertRedirects(
-            response,
-            reverse(
-                'posts:post_detail', kwargs={'post_id': PostURLTests.post.id}))
-
-    def test_post_edit_page_redirect_anonymous_on_login_page(self):
-        """
-        Страница /<post_id>/edit/ перенаправит анонимного пользователя
-        на страницу логина.
-        """
-        post_edit_page = reverse(
-            'posts:post_edit', kwargs={'post_id': PostURLTests.post.id})
-        response = self.post_author.get(post_edit_page)
-        self.assertRedirects(
-            response,
-            f"{reverse('users:login')}?next={post_edit_page}")
 
     def test_unexisting_page_is_unavailable(self):
         """Несуществующая страница недоступна (возвращает статус 404)."""
